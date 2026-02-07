f0d113cc996d7283a080268abaca9ffeb88ba5d5
LAB13_USER ON WS22 <L13Y17W22@LabHCI.net>
2026-02-07 16:31:45 +0800

    Setup repository structure

---
+++ backend/src/main/java/IT342_G6_Maluto_Lab1/It342G6MalutoLab1Application.java
@@ -0,0 +1,13 @@
+package IT342_G6_Maluto_Lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It342G6MalutoLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It342G6MalutoLab1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/IT342_G6_Maluto_Lab1/It342G6MalutoLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package IT342_G6_Maluto_Lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It342G6MalutoLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
